82b76cf6a1f0b0aa775f57cc3c26102e5601b2a5
kbinani <kbinani@msn.com>
2013-02-24 21:38:10 +0900

    Add MeshRepository class

---
+++ MeshRepository.cpp
@@ -0,0 +1,20 @@
+﻿#include "MeshRepository.h"
+
+namespace com { namespace github { namespace kbinani {
+
+    MeshRepository *MeshRepository::Instance() {
+        static MeshRepository instance;
+        return &instance;
+    }
+
+    void MeshRepository::Register(const MeshDescriptor &descriptor) {
+        if (!Exists(descriptor)) {
+            repository.insert(std::make_pair(descriptor.Hash(), descriptor));
+        }
+    }
+
+    bool MeshRepository::Exists(const MeshDescriptor &descriptor) {
+        return repository.find(descriptor.Hash()) != repository.end();
+    }
+
+} } }
---
+++ MeshRepository.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Main.h"
+
+namespace com { namespace github { namespace kbinani {
+
+    class MeshRepository {
+    public:
+        static MeshRepository *Instance();
+
+        void Register(const MeshDescriptor &descriptor);
+
+        bool Exists(const MeshDescriptor &descriptor);
+
+    private:
+        std::map<std::string, MeshDescriptor> repository;
+    };
+
+} } }
