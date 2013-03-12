6134415f97ae7e5af8a33a9b616eea406dd1c0c0
kbinani <kbinani@msn.com>
2013-03-13 08:29:47 +0900

    Refactor 'Init' class

--- src/init.py
+++ src/init.py
@@ -3,17 +3,27 @@ import os
 import dxrip
 
 class Init:
+    __target = None
+
     def execute(self, arguments):
+        self.__parse_arguments(arguments)
+        self.__validate_options()
+        self.__prepare_repository_directory()
+
+    def __parse_arguments(self, arguments):
         parser = argparse.ArgumentParser()
         parser.add_argument('target')
         args = parser.parse_args(arguments)
-        target = args.target
-        if target is None:
-            raise dxrip.Error([])
-        if not os.path.isfile(target):
-            raise dxrip.Error(['error: file "' + target + '" does not exists'])
+        self.__target = args.target
 
-        directory = os.path.dirname(target)
+    def __validate_options(self):
+        if self.__target is None:
+            raise dxrip.Error([])
+        if not os.path.isfile(self.__target):
+            raise dxrip.Error(['error: file "' + self.__target + '" does not exists'])
+
+    def __prepare_repository_directory(self):
+        directory = os.path.dirname(self.__target)
         repository = os.path.join(directory, '.dxrip')
         if os.path.isdir(repository):
             raise dxrip.Error(['error: ".dxrip" directory already exists'])
