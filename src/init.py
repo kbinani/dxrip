820881225cf45ac12e92acaaf4a18979f7664e01
kbinani <kbinani@msn.com>
2013-03-14 00:15:26 +0900

    Modify init.py, refactor to use dxrip.lib.Config class

--- src/init.py
+++ src/init.py
@@ -3,7 +3,7 @@ import os
 import dxrip
 
 class Init:
-    __target = None
+    __config = None
 
     def execute(self, arguments):
         self.__parse_arguments(arguments)
@@ -14,16 +14,17 @@ class Init:
         parser = argparse.ArgumentParser()
         parser.add_argument('target')
         args = parser.parse_args(arguments)
-        self.__target = args.target
+        self.__config = dxrip.lib.Config(os.path.dirname(args.target))
+        self.__config.target = args.target
 
     def __validate_options(self):
-        if self.__target is None:
+        if self.__config.target is None:
             raise dxrip.Error([])
-        if not os.path.isfile(self.__target):
-            raise dxrip.Error(['error: file "' + self.__target + '" does not exists'])
+        if not os.path.isfile(self.__config.target):
+            raise dxrip.Error(['error: file "' + self.__config.target + '" does not exists'])
 
     def __prepare_repository_directory(self):
-        directory = os.path.dirname(self.__target)
+        directory = os.path.dirname(self.__config.target)
         repository = os.path.join(directory, '.dxrip')
         if os.path.isdir(repository):
             raise dxrip.Error(['error: ".dxrip" directory already exists'])
