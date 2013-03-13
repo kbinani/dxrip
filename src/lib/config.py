1f76b11cbc6c2ad0b5e8daf40f4ccc93e0d0c6ff
kbinani <kbinani@msn.com>
2013-03-13 20:54:50 +0900

    Add lib.Config.write method

--- src/lib/config.py
+++ src/lib/config.py
@@ -7,13 +7,27 @@ class Config:
 
     def __init__(self, directory):
         self.__directory = directory
-        self.__read(self.__directory)
+        conf_path = self.__get_conf_path()
+        if os.path.isfile(conf_path):
+            self.__read(conf_path)
 
     def target(self):
         return self.__target
 
-    def __read(self, directory):
+    def set_target(self, target):
+        self.__target = target
+
+    def write(self):
+        conf_path = self.__get_conf_path()
+        contents = ["[core]", "\ttarget = " + self.__target]
+        file = open(conf_path, "w")
+        file.writelines([line + "\n" for line in contents])
+        file.close()
+
+    def __read(self, conf_path):
         conf = configparser.SafeConfigParser()
-        conf_path = os.path.join(directory, '.dxrip', 'config')
         conf.read(conf_path)
-        self.__target = conf.get('core', 'target')
+        self.__target = conf.get("core", "target")
+
+    def __get_conf_path(self):
+        return os.path.join(self.__directory, ".dxrip", "config")
--- src/test/lib/test_config.py
+++ src/test/lib/test_config.py
@@ -2,14 +2,26 @@ import xmlrunner
 import unittest
 import tempfile
 import dxrip
+import os
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_new(self):
-        config = dxrip.lib.Config('test/fixture/lib/test_config')
-        self.assertEqual('C:\Program Files\Vendor Name\Hoge.exe', config.target())
+        config = dxrip.lib.Config("test/fixture/lib/test_config")
+        self.assertEqual("C:\Program Files\Vendor Name\Hoge.exe", config.target())
 
-if __name__ == '__main__':
-    unittest.main(testRunner = xmlrunner.XMLTestRunner(output = 'test/report'))
+    def test_write(self):
+        directory = tempfile.mkdtemp()
+        os.mkdir(os.path.join(directory, ".dxrip"))
+        config = dxrip.lib.Config(directory)
+        config.set_target("Hoge.exe")
+        config.write()
+
+        conf_path = os.path.join(directory, ".dxrip", "config")
+        self.assertTrue(os.path.isfile(conf_path))
+        self.assertEqual(["[core]", "\ttarget = Hoge.exe"], [line.rstrip() for line in open(conf_path, "r").readlines()])
+
+if __name__ == "__main__":
+    unittest.main(testRunner = xmlrunner.XMLTestRunner(output = "test/report"))
