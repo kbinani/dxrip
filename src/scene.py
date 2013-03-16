3f0ca37258ebb825ddac8e12a9bad346f6f20584
kbinani <kbinani@msn.com>
2013-03-17 02:59:33 +0900

    Modify scene.py, refactor

--- src/scene.py
+++ src/scene.py
@@ -5,39 +5,45 @@ import tempfile
 import shutil
 
 class Scene:
-
     def execute(self, arguments):
         config = dxrip.lib.Config(os.getcwd())
+        scene = self.__get_scene_number(arguments)
+        x_file_list = self.__get_x_file_list(config.get_scene_directory_path(), scene)
+        temporary_script_path = self.__create_temporary_script(x_file_list)
+        self.__execute_temporary_script(config.blender, temporary_script_path)
 
+    def __get_scene_number(self, arguments):
         parser = argparse.ArgumentParser()
         parser.add_argument('scene')
         args = parser.parse_args(arguments)
-        scene = int(args.scene)
+        return int(args.scene)
 
-        scene_directory = os.path.join(config.get_scene_directory_path(), str(scene))
+    def __get_x_file_list(self, scene_directory_path, scene):
+        scene_directory = os.path.join(scene_directory_path, str(scene))
         if not os.path.isdir(scene_directory):
             raise dxrip.Error(["error: scene " + str(scene) + " does not exist"])
 
         all_file_list = [os.path.join(scene_directory, file) for file in os.listdir(scene_directory)]
-        x_file_list = [file.replace("\\", "\\\\") for file in all_file_list if os.path.isfile(file) and os.path.splitext(file)[1] == ".x"]
+        return [file.replace("\\", "\\\\") for file in all_file_list if os.path.isfile(file) and os.path.splitext(file)[1] == ".x"]
+
+    def __create_temporary_script(self, x_file_list):
+        file_list = "file_list = [\"" + "\", \"".join(x_file_list) + "\"]"
 
         data_directory = os.path.join(os.path.dirname(dxrip.__file__), "data")
         template_path = os.path.join(data_directory, "ximporter.py.template")
-        template_file = open(template_path, "r")
-        template_contents = template_file.read()
-        template_file.close()
-        file_list = "file_list = [\"" + "\", \"".join(x_file_list) + "\"]"
-        contents = template_contents.replace("#<file_list>#", file_list)
-        status, temporary_script_path = tempfile.mkstemp()
+        with open(template_path, "r") as template_file:
+            contents = template_file.read().replace("#<file_list>#", file_list)
+        fd, outfile_path = tempfile.mkstemp()
+        with os.fdopen(fd, "w") as outfile:
+            outfile.write(contents)
+        return outfile_path
 
-        temporary_script_file = open(temporary_script_path, "w")
-        temporary_script_file.write(contents)
-        temporary_script_file.close()
-
+    def __execute_temporary_script(self, blender, temporary_script_path):
+        data_directory = os.path.join(os.path.dirname(dxrip.__file__), "data")
         fd, empty_blend_path = tempfile.mkstemp()
         os.fdopen(fd, "w").close()
         os.remove(empty_blend_path)
         shutil.copyfile(os.path.join(data_directory, "empty.blend"), empty_blend_path)
 
-        command = '"' + config.blender + '" ' + empty_blend_path + ' --python ' + temporary_script_path
+        command = '"' + blender + '" ' + empty_blend_path + ' --python ' + temporary_script_path
         os.system(command)
